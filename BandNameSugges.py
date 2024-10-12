59c1ffaea260e320953971a791f66bbfde5581e9
Talha Shahid <2022CS630@student.uet.edu.pk>
2024-10-12 12:16:16 +0500

    Created a band name suggestor simply from input.

---
+++ BandNameSugges.py
@@ -0,0 +1,6 @@
+print("Welcome to Band Name Generator.")
+
+city = input("What's name of the city you grew up?\n")
+pet_name = input("What's the name of your pet?\n")
+
+print("The Band name can be", city , pet_name, ".")
